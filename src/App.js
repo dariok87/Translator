d028d0af330c4a00e253b1daf162ea4b375f25e9
dariok87 <dkemenovic@gmail.com>
2022-02-09 18:40:30 +0100

    Import components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Field from './components/field';
+import Languages from './components/languages';
+import Translate from './components/translate';
 
 export default function App() {
   return (
     <div>
-      <h1 className='title'>
-        hi there !
+      <h1>
+        <Field />
+        <Languages />
+        <hr />
+        <Translate />
       </h1>
     </div>
   );
